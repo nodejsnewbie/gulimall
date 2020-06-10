e3ba6465909608971013a11201b0b72d3cf7dc19
cuiyue <cuiyue@chehejia.com>
2020-06-10 17:03:27 +0800

    分组修改

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/entity/AttrGroupEntity.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/entity/AttrGroupEntity.java
@@ -1,15 +1,17 @@
 package com.atguigu.gulimall.product.entity;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 
 import java.io.Serializable;
 import java.util.Date;
+
 import lombok.Data;
 
 /**
  * 属性分组
- * 
+ *
  * @author cuiyue
  * @email 380920705@qq.coom
  * @date 2020-05-13 10:51:11
@@ -17,32 +19,38 @@ import lombok.Data;
 @Data
 @TableName("pms_attr_group")
 public class AttrGroupEntity implements Serializable {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * 分组id
-	 */
-	@TableId
-	private Long attrGroupId;
-	/**
-	 * 组名
-	 */
-	private String attrGroupName;
-	/**
-	 * 排序
-	 */
-	private Integer sort;
-	/**
-	 * 描述
-	 */
-	private String descript;
-	/**
-	 * 组图标
-	 */
-	private String icon;
-	/**
-	 * 所属分类id
-	 */
-	private Long catelogId;
+    /**
+     * 分组id
+     */
+    @TableId
+    private Long attrGroupId;
+    /**
+     * 组名
+     */
+    private String attrGroupName;
+    /**
+     * 排序
+     */
+    private Integer sort;
+    /**
+     * 描述
+     */
+    private String descript;
+    /**
+     * 组图标
+     */
+    private String icon;
+    /**
+     * 所属分类id
+     */
+    private Long catelogId;
+
+    /**
+     * 数据库中没有的字段
+     */
+    @TableField(exist = false)
+    private Long[] catelogPath;
 
 }
--- gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/service/impl/CategoryServiceImpl.java
@@ -4,10 +4,7 @@ import com.atguigu.common.utils.PageUtils;
 import com.atguigu.common.utils.Query;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
@@ -49,6 +46,25 @@ public class CategoryServiceImpl extends ServiceImpl<CategoryDao, CategoryEntity
         //TODO  1、检查当前删除的菜单，是否被别的地方引用
         //逻辑删除
         baseMapper.deleteBatchIds(asList);
+    }
+
+    @Override
+    public Long[] findCatelogPath(Long catelogId) {
+        List<Long> paths = new ArrayList<>();
+        List<Long> parentPath = findParentPath(catelogId, paths);
+        Collections.reverse(parentPath);
+        return parentPath.toArray(new Long[parentPath.size()]);
+    }
+
+    //225,25,2
+    private List<Long> findParentPath(Long catelogId, List<Long> paths) {
+        //1、收集当前节点id
+        paths.add(catelogId);
+        CategoryEntity byId = this.getById(catelogId);
+        if (byId.getParentCid() != 0) {
+            findParentPath(byId.getParentCid(), paths);
+        }
+        return paths;
     }
 
     /**
