19b82f17c35cd44a5a08ea04a67b55bf97d3d6ad
cuiyue <cuiyue@chehejia.com>
2020-06-16 10:15:43 +0800

    增加仓库查询

--- gulimall-ware/src/main/java/com/atguigu/gulimall/ware/controller/WareInfoController.java
+++ gulimall-ware/src/main/java/com/atguigu/gulimall/ware/controller/WareInfoController.java
@@ -16,7 +16,6 @@ import com.atguigu.common.utils.PageUtils;
 import com.atguigu.common.utils.R;
 
 
-
 /**
  * 仓库信息
  *
@@ -34,9 +33,8 @@ public class WareInfoController {
      * 列表
      */
     @RequestMapping("/list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = wareInfoService.queryPage(params);
-
         return R.ok().put("page", page);
     }
 
@@ -45,9 +43,8 @@ public class WareInfoController {
      * 信息
      */
     @RequestMapping("/info/{id}")
-    public R info(@PathVariable("id") Long id){
-		WareInfoEntity wareInfo = wareInfoService.getById(id);
-
+    public R info(@PathVariable("id") Long id) {
+        WareInfoEntity wareInfo = wareInfoService.getById(id);
         return R.ok().put("wareInfo", wareInfo);
     }
 
@@ -55,9 +52,8 @@ public class WareInfoController {
      * 保存
      */
     @RequestMapping("/save")
-    public R save(@RequestBody WareInfoEntity wareInfo){
-		wareInfoService.save(wareInfo);
-
+    public R save(@RequestBody WareInfoEntity wareInfo) {
+        wareInfoService.save(wareInfo);
         return R.ok();
     }
 
@@ -65,9 +61,8 @@ public class WareInfoController {
      * 修改
      */
     @RequestMapping("/update")
-    public R update(@RequestBody WareInfoEntity wareInfo){
-		wareInfoService.updateById(wareInfo);
-
+    public R update(@RequestBody WareInfoEntity wareInfo) {
+        wareInfoService.updateById(wareInfo);
         return R.ok();
     }
 
@@ -75,9 +70,8 @@ public class WareInfoController {
      * 删除
      */
     @RequestMapping("/delete")
-    public R delete(@RequestBody Long[] ids){
-		wareInfoService.removeByIds(Arrays.asList(ids));
-
+    public R delete(@RequestBody Long[] ids) {
+        wareInfoService.removeByIds(Arrays.asList(ids));
         return R.ok();
     }
 
