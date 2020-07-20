412f2d302c7fc05cca1f9f6a0151a39de91c572b
cuiyue <cuiyue@chehejia.com>
2020-07-20 17:20:58 +0800

    发送验证码

--- gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/controller/LoginController.java
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/controller/LoginController.java
@@ -1,9 +1,32 @@
 package com.atguigu.gulimall.auth.controller;
 
+import com.atguigu.common.utils.R;
+import com.atguigu.gulimall.auth.feign.ThirdParFeignService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LoginController {
+
+    @Autowired
+    ThirdParFeignService thirdParFeignService;
+
+    @ResponseBody
+    @GetMapping("sms/sendCode")
+    public R sendCode(@RequestParam("phone") String phone) {
+
+        //1.接口防刷
+
+        //2.验证验证码
+
+
+        int radomInt = (int) ((Math.random() * 9 + 1) * 100000);
+        String code = String.valueOf(radomInt);
+        thirdParFeignService.sendCode(phone, code);
+        return R.ok();
+    }
 
 }
--- gulimall-auth-server/src/test/java/com/atguigu/gulimall/auth/GulimallAuthServerApplicationTests.java
+++ gulimall-auth-server/src/test/java/com/atguigu/gulimall/auth/GulimallAuthServerApplicationTests.java
@@ -3,11 +3,21 @@ package com.atguigu.gulimall.auth;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.Random;
+import java.util.UUID;
+
 @SpringBootTest
 class GulimallAuthServerApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testCode() {
+        int radomInt = (int) ((Math.random() * 9 + 1) * 100000);
+        String radomString = String.valueOf(radomInt);
+        System.out.println(radomString);
     }
 
 }
