a781b99aafcad6d118a21b0a9f0c0eba97d1de10
cuiyue <cuiyue@chehejia.com>
2020-07-20 15:12:38 +0800

    修改登录注册跳转

--- gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/controller/LoginController.java
+++ gulimall-auth-server/src/main/java/com/atguigu/gulimall/auth/controller/LoginController.java
@@ -6,14 +6,4 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class LoginController {
 
-    @GetMapping("/login.html")
-    public String loginPage() {
-        return "login";
-    }
-
-    @GetMapping("/reg.html")
-    public String regPage() {
-        return "reg";
-    }
-
 }
