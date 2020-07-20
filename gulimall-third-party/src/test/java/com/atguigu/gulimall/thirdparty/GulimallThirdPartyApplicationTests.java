65ff3e2dbe281b932d9b88007127cf7b5fa5031e
cuiyue <cuiyue@chehejia.com>
2020-07-20 15:49:18 +0800

    发送验证码测试代码

--- gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
+++ gulimall-third-party/src/test/java/com/atguigu/gulimall/thirdparty/GulimallThirdPartyApplicationTests.java
@@ -1,12 +1,18 @@
 package com.atguigu.gulimall.thirdparty;
 
 import com.aliyun.oss.OSSClient;
+import com.atguigu.gulimall.thirdparty.component.SmsComponent;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.List;
+import java.util.Map;
 
 
 @SpringBootTest
@@ -15,8 +21,11 @@ class GulimallThirdPartyApplicationTests {
     @Autowired
     OSSClient ossClient;
 
+    @Autowired
+    SmsComponent smsComponent;
+
     @Test
-    public void testUpload()  {
+    public void testUpload() {
         try {
             ossClient.putObject("gulimall-cy", "ha.jpg", new FileInputStream("/Users/cuiyue/Desktop/abc.jpg"));
             System.out.println("上传完成");
@@ -24,4 +33,10 @@ class GulimallThirdPartyApplicationTests {
             e.printStackTrace();
         }
     }
+
+    @Test
+    public void testSendSms() {
+        smsComponent.sendSmsCode("18513027009","135246");
+    }
+
 }
