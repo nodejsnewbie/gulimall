4a68ca08ebc81f40162712afceffd454ed331a92
cuiyue <cuiyue@chehejia.com>
2020-08-10 14:13:35 +0800

    登录状态展示

--- gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
+++ gulimall-order/src/main/java/com/atguigu/gulimall/order/GulimallOrderApplication.java
@@ -4,10 +4,13 @@ import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@SpringBootApplication
+
 @EnableDiscoveryClient
 @EnableRabbit
+@EnableRedisHttpSession
+@SpringBootApplication
 public class GulimallOrderApplication {
 
     public static void main(String[] args) {
